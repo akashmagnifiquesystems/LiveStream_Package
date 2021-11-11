a9306d275df6a07a9d1208bd7e7120cbd5fc9a25
Akash <aakashpatel.iosdev@gmail.com>
2021-11-11 21:25:49 +0530

    Update

--- Sources/LiveStream_Package/LiveStream_Package.swift
+++ Sources/LiveStream_Package/LiveStream_Package.swift
@@ -7,7 +7,7 @@ public struct LiveStream_Package {
     public init() {
     }
     
-    func getStreams(searchValue : String,  completion: @escaping ([Livestream], [Upcomingstream]) -> Void)
+    public func getStreams(searchValue : String,  completion: @escaping ([Livestream], [Upcomingstream]) -> Void)
     {
         LiveStreamViewModel.shared.getStreams(SearchValue: searchValue, completion: {liveStream,Upcomingstream in
             completion(liveStream, Upcomingstream)
